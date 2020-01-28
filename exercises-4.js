4155e69e1dff3abfb7026aa42894266d01c0661b
susilo hartomo <encusilo545110@gmail.com>
2020-01-28 19:08:24 +0700

    update exercises-4

--- exercises-4.js
+++ exercises-4.js
@@ -1,5 +1,5 @@
-var tanggal = 30; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
-var bulan = 1; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tanggal = 33; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = 13; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
 var tahun = 1800; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
 
 if (tanggal < 32) {
@@ -61,55 +61,3 @@ if (tanggal < 32) {
     console.log('Isi tanggal dengan angka antara 1 - 31')
 }
 
-
-
-
-// switch (bulan) {
-//     case '1':
-//         bulan = 'Januari'
-//         console.log(tanggal + ' ' + bulan + ' ' + tahun)
-//         break;
-//     case '2':
-//         bulan = 'Februari'
-//         console.log(tanggal + ' ' + bulan + ' ' + tahun)
-//         break;
-//     case '3':
-//         bulan = 'Maret'
-//         console.log(tanggal + ' ' + bulan + ' ' + tahun)
-//         break;
-//     case '4':
-//         bulan = 'April'
-//         console.log(tanggal + ' ' + bulan + ' ' + tahun)
-//         break;
-//     case '5':
-//         bulan = 'Mei'
-//         console.log(tanggal + ' ' + bulan + ' ' + tahun)
-//         break;
-//     case '7':
-//         bulan = 'Juni'
-//         console.log(tanggal + ' ' + bulan + ' ' + tahun)
-//         break;
-//     case '8':
-//         bulan = 'Juli'
-//         console.log(tanggal + ' ' + bulan + ' ' + tahun)
-//         break;
-//     case '9':
-//         bulan = 'Agustus'
-//         console.log(tanggal + ' ' + bulan + ' ' + tahun)
-//         break;
-//     case '10':
-//         bulan = 'September'
-//         console.log(tanggal + ' ' + bulan + ' ' + tahun)
-//         break;
-//     case '11':
-//         bulan = 'November'
-//         console.log(tanggal + ' ' + bulan + ' ' + tahun)
-//         break;
-//     case '12':
-//         bulan = 'Desember'
-//         console.log(tanggal + ' ' + bulan + ' ' + tahun)
-//         break;
-//     default:
-//         console.log('nilai variabel tanggal disini! (dengan angka antara 1 - 31) \nnilai variabel bulan disini! (dengan angka antara 1 - 12) \nnilai variabel tahun disini! (dengan angka antara 1900 - 2200)')
-//         break;
-// }
